daa99987941d35ce7e7281ac5a245622b6d78e2a
akari01 <36139666+akari01@users.noreply.github.com>
2019-09-25 14:53:19 +1000

    Update BoringStuff.py
    first practice from boring stuff

--- BoringStuff.py
+++ BoringStuff.py
@@ -1,3 +1,12 @@
-#python
-#! python3
+#! python3 practice 1
+passwordFile = open('SecretPasswordFile.txt')
+secretPassword = passwordFile.read()
+print('Enter Password')
+userpass = input()
+if userpass == secretPassword:
+  print('Access granted')
+  if userpass == '12345':
+    print('That password is one that an idiot outs on their luggage.')
+ else:
+  print('Access denied')
 
